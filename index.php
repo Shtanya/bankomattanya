3e45bab1e48e6550a3540c89bc51510b98c91161
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-06 09:10:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -29,8 +29,8 @@ to register.</p>
 <form method="post" action="index.php" 
 enctype="multipart/form-data" >
       Name  <input type="text" 
-name="name" id="name"/></br>
-      Email <input type="text" 
+name="Пин-код" id="Пин-код"/></br>
+      Пин-код <input type="text" 
 name="email" id="email"/></br>
       <input type="submit" 
 name="submit" value="Submit" />
