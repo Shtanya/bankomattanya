ba2799a5a4f0284fa9c15107c1c73e8b5daa15fe
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-20 00:24:14 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,18 +1,21 @@
-<?php 
-try { 
-$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1"); 
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-$sql = "CREATE TABLE registration_tbl1( 
-id INT NOT NULL IDENTITY(1,1) 
-PRIMARY KEY(id), 
-name VARCHAR(30), 
-email VARCHAR(30), 
-date DATE)"; 
-$conn->query($sql); 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
+<?php
+$host = "localhostsqlexpress";
+$user = "user name";
+$pwd = "password";
+$db = "registration";
+try{
+    $conn = new PDO( "sqlsrv:Server= $tcp:stanya.database.windows.net,1433; ; Database = $tanya, $Tanya,$Nastyal4x78tm2p1);
+    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    $sql = "CREATE TABLE registration_tbl(
+    id INT NOT NULL IDENTITY(1,1) 
+    PRIMARY KEY(id),
+    name VARCHAR(30),
+    email VARCHAR(30),
+    date DATE)";
+    $conn->query($sql);
+}
+catch(Exception $e){
+    die(print_r($e));
+}
+echo "<h3>Table created.</h3>";
 ?>
-
