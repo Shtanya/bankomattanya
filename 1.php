c14a3071b35ea3e446750086ebb991588bcf271d
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-19 22:05:55 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -1,10 +1,8 @@
 <html>
 <head>
 <Title>Registration Form</Title>
-<style type="text/css">
-background-color:#243CED; color:green; 
-</style>
 </head>
+<body style="color:Yellow; background-color:#66cc66"> 
 <body>
 <img src='http://data.sberbank.ru/common/img/uploaded/banks/uploaded_szb/files/smi_sber_logo_vsegda_695.jpg'>";    
 <h1>Выбор операции "Операции с наличными"</h1>
