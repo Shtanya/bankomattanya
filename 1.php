67e679837d21cfe178912d9f1e4e66089ce9a670
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-19 22:20:47 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,7 +4,7 @@
 </head>
 <body style="color:Black; background-color:#66cc66"> 
 <body>
-<img src='http://data.sberbank.ru/common/img/uploaded/banks/uploaded_szb/files/smi_sber_logo_vsegda_695.jpg';    
+<img src='http://data.sberbank.ru/common/img/uploaded/banks/uploaded_szb/files/smi_sber_logo_vsegda_695.jpg'  
 <h1>Выбор операции "Операции с наличными"</h1>
 <form method="post" action="1.php" 
 enctype="multipart/form-data" >
