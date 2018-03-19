3710df8e289bb99ea5d07a9a5d74be00bfd4ffcd
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-20 00:04:50 +0300

    Create 3.php

---
+++ 3.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<Title>Registration Form</Title>
+<body style="color:Black; background-color:#66cc66"> 
+<img src='http://novostroiki-irkutsk.ru/images/logo_sberbank.png'>
+</head>
+<body>
+<h1>Нужен ли вам чек?</h1>
+<form method="post" action="3.php" 
+enctype="multipart/form-data" >
+<input type="submit" 
+name="Да" value="Да" />
+<input type="submit" 
+name="Нет" value="Нет" />
+</form>
+<?php
+
+?>
+</body>
+</html>
