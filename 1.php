a6845c0cf0ed77dcc19bb896a543b64fa360f1c0
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-19 21:34:13 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -2,27 +2,11 @@
 <head>
 <Title>Registration Form</Title>
 <style type="text/css">
-    body { background-color:
- #fff; border-top: solid 10px #000;
- color: #333; font-size: .85em;
- margin: 20; padding: 20;
- font-family: "Segoe UI",
- Verdana, Helvetica, Sans-Serif;
-    }
-    h1, h2, h3,{ color: #000; 
-margin-bottom: 0; padding-bottom: 0; }
-    h1 { font-size: 2em; }
-    h2 { font-size: 1.75em; }
-    h3 { font-size: 1.2em; }
-    table { margin-top: 0.75em; }
-    th { font-size: 1.2em;
- text-align: left; border: none; padding-left: 0; }
-    td { padding: 0.25em 2em 0.25em 0em; 
-border: 0 none; }
+background-color:#243CED; color:green; 
 </style>
 </head>
 <body>
-$a = "<img src='http://data.sberbank.ru/common/img/uploaded/banks/uploaded_szb/files/smi_sber_logo_vsegda_695.jpg'>";    
+<img src='http://data.sberbank.ru/common/img/uploaded/banks/uploaded_szb/files/smi_sber_logo_vsegda_695.jpg'>";    
 <h1>Выбор операции "Операции с наличными"</h1>
 <form method="post" action="1.php" 
 enctype="multipart/form-data" >
