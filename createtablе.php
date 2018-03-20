023ebabc7116d243e72188c9183f3e6acc610f66
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-20 11:11:56 +0200

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,16 +1,18 @@
 <?php
-$host = "localhostsqlexpress";
-$user = "user name";
-$pwd = "password";
-$db = "registration";
+$host = "stanya.database.windows.net,1433";
+$user = "Tanya";
+$pwd = "Nastyal4x78tm2p1";
+$db = "tanya";
 try{
     $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1");
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
     $sql = "CREATE TABLE registration_tbl(
     id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
+    PRIMARY KEY(nomer karty),
+    "pin-kod" VARCHAR(30),
+    "srok karty" VARCHAR(30),
+    "balans" VARCHAR(30),
+    "f.i.o." VARCHAR(30),
     date DATE)";
     $conn->query($sql);
 }
