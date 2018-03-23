87fd2a89493236d0a95fa63e20a6292e6171a851
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-23 14:36:22 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,25 +1,17 @@
-<?php
-$host = "stanya.database.windows.net,1433";
-$user = "Tanya";
-$pwd = "Nastyal4x78tm2p1";
-$db = "tanya";
-try{
-    $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1");
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1)
-    CREATE TABLE Karty
-    PRIMARY KEY(id),
-    "nomer karty" VARCHAR(30),
-    "pin-kod" VARCHAR(30),
-    "srok karty" VARCHAR(30),
-    "balans" VARCHAR(30),
-    "f.i.o." VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-}
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
+<?php 
+try { 
+$conn = new PDO("sqlsrv:server = tcp:pinyasova.database.windows.net,1433; Database = Progr", "Valera", "hswfhmlyz08"); 
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+$sql = "CREATE TABLE registration_tbl1( 
+id INT NOT NULL IDENTITY(1,1) 
+PRIMARY KEY(id), 
+name VARCHAR(30), 
+email VARCHAR(30), 
+date DATE)"; 
+$conn->query($sql); 
+} 
+catch (PDOException $e) { 
+print("Error connecting to SQL Server."); 
+die(print_r($e)); 
+} 
 ?>
