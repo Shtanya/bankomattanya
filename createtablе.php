bf024604fc9e18f4628b93e9410e10f04ac2dd29
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-03-25 15:31:30 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -6,7 +6,7 @@ $db = "tanya";
 try{
     $conn = new PDO( "sqlsrv:Server=  stanya.database.windows.net ; Database = tanya ", Tanya, Nastyal4x78tm2p1);
     $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
+    $sql = "CREATE TABLE bankomat_tbl(
     id INT NOT NULL IDENTITY(1,1) 
     PRIMARY KEY(id),
     name VARCHAR(30),
