76939ddb232cb4931efac2aa1dfdfd4e48a7850c
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 16:31:19 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -11,12 +11,15 @@ Srok karti varchar(30) NOT NULL AUTO_INCREMENT,
 Balans varchar(30) NOT NULL AUTO_INCREMENT, 
 Familiya varchar(30) NOT NULL AUTO_INCREMENT, 
 Imya varchar(30) NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY (Nomer karti); 
-$conn->query($sql); 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-} 
-echo"123"; 
-?>
+PRIMARY KEY (Nomer karti)
+) ";
+$sql="CREATE TABLE Operacii ( 
+Data DATE(30) NOT NULL, 
+Vremya DATETIME(30) NOT NULL, 
+Symma varchar(30) NOT NULL, 
+Nomer karti varchar(30) NOT NULL AUTO_INCREMENT, 
+PRIMARY KEY (Nomer karti) 
+)"; 
+mysql_query($sql) or die(mysql_error());
+mysql_close();
+print '<p>Data Base created!</p>';
