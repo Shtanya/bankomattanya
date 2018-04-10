d1016c0ea24e6c19c17aaa6a91eab6e08421544d
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 18:30:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ name="Номер карты" id="Номер карты"/></br>
 Срок действия карты <input type="text" 
 name="Срок действия карты" id="Срок действия карты"/></br>
 <FONT color="red">Когда карта просрочена,она блокируется,и денежные средства с нее получить нельзя.</FONT> 
-<p>Введите пароль,после набора нажмите  на <strong>Далее</strong> </p>
+<p>Введите пароль,после набора нажмите  на <strong>Продолжить</strong> </p>
 <p>Для отказа от операции нажмите  <strong>Отмена</strong> </p>
 <form method="post" action="index.php" 
 enctype="multipart/form-data" >
@@ -22,7 +22,7 @@ enctype="multipart/form-data" >
     <input name="password" type="password" size="15" maxlength="15">
     </p>
 <form>
-<input type="button" value="Далее" onClick='location.href="1.php"'>
+<input type="button" value="Продолжить" onClick='location.href="1.php"'>
 </form>
 <form method="POST"> 
 <input type="submit" name="nazvanie_knopki" value="Отмена" /> 
