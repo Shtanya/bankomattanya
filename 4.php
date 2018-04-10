3b918e3efab71a1f730196051ed4f2a95ee287b1
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 17:23:15 +0300

    Update 4.php

--- 4.php
+++ 4.php
@@ -1,6 +1,5 @@
 <html>
 <head>
-<meta charset="itf-8">
 <Title>Registration Form</Title>
 <body style="color:Black; background-color:#66cc66"> 
 <img src='http://novostroiki-irkutsk.ru/images/logo_sberbank.png'>
