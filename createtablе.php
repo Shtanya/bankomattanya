b0f04c6ca919bc29697eae127152052c140c4ab7
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 19:57:30 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,5 +1,4 @@
-<?php 
-echo "123456"; 
+<?php  
 try { 
 $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1") 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
@@ -14,19 +13,10 @@ Familiya varchar(30) NOT NULL AUTO_INCREMENT,
 Imya varchar(30) NOT NULL AUTO_INCREMENT, 
 PRIMARY KEY (id)
 ) ";
-$sql="CREATE TABLE Operacii ( 
-id int(11) NOT NULL auto_increment,
-Data DATE(30) NOT NULL, 
-Vremya DATETIME(30) NOT NULL, 
-Symma varchar(30) NOT NULL, 
-Nomer karti varchar(30) NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY (id) 
-)"; 
 $conn->query($sql); 
 } 
 catch (PDOException $e) { 
 print("Error connecting to SQL Server."); 
 die(print_r($e)); 
-} 
-echo"123"; 
+}  
 ?>
