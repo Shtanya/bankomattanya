384844a988478083e75444b282b173aeada79def
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 19:19:31 +0300

    Update 3.php

--- 3.php
+++ 3.php
@@ -8,12 +8,13 @@
 <h1>Нужен ли вам чек?</h1>
 <form method="post" action="3.php" 
 enctype="multipart/form-data" >
-<form>
+<form>  
 <input type="button" value="Да" onClick='location.href="4.php"'>
 </form>
 <input type="submit" name="submit" value="Нет"> 
 <?php
-if (isset($_POST['submit'])) { 
+if (isset($_POST['submit']))
+{ 
 echo "Заберите карту,и деньги!"; 
 }   
 ?>
