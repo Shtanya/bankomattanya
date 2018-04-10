bac7ebc84994ae0e6104fe7ec6cfd5f1d6f21d27
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-10 17:08:15 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -4,21 +4,23 @@ try {
 $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database stanya", "Tanya", "Nastyal4x78tm2p1") 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
 
-$sql = "CREATE TABLE Karta ( 
+$sql = "CREATE TABLE Karta (
+id int(11) NOT NULL auto_increment,
 Nomer karti varchar(30) NOT NULL AUTO_INCREMENT, 
 Pin-kod varchar(30) NOT NULL AUTO_INCREMENT, 
 Srok karti varchar(30) NOT NULL AUTO_INCREMENT, 
 Balans varchar(30) NOT NULL AUTO_INCREMENT, 
 Familiya varchar(30) NOT NULL AUTO_INCREMENT, 
 Imya varchar(30) NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY (Nomer karti)
+PRIMARY KEY (id)
 ) ";
 $sql="CREATE TABLE Operacii ( 
+id int(11) NOT NULL auto_increment,
 Data DATE(30) NOT NULL, 
 Vremya DATETIME(30) NOT NULL, 
 Symma varchar(30) NOT NULL, 
 Nomer karti varchar(30) NOT NULL AUTO_INCREMENT, 
-PRIMARY KEY (Nomer karti) 
+PRIMARY KEY (id) 
 )"; 
 $conn->query($sql); 
 } 
