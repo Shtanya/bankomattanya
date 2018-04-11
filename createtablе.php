86c9986e2c7bed7a4860c9c40456027b3b9efd75
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 19:36:55 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,31 +1,27 @@
 <?php
-echo "Снятие наличных"; 
-try { 
-$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1");
-$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
- 
+try {
+$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya","L4x78tm2p1");
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE Karta (
 id INT NOT NULL IDENTITY(1,1)
 PRIMARY KEY (id),
 Nomer karti varchar(30),
 Password varchar(30),
 Srok karti varchar(30),
-Balans varchar(30))"; 
-$conn->query($sql);
+Balans varchar(30))";
 )";
-
 $sql = "CREATE TABLE Operacii (
 id INT NOT NULL IDENTITY(1,1)
 PRIMARY KEY (id),
 Data DATETIME(30),
 Vremya DATETIME(30),
 Summa varchar(100))";
+)";
 $conn->query($sql); 
-)"; 
-} 
-catch (PDOException $e) { 
-print("Error connecting to SQL Server."); 
-die(print_r($e)); 
-}  
-echo"Снятие наличных";
+echo "<h3>Таблица создана!</h3>";
+}
+catch (PDOException $e) {
+print("Ошибка подключения к SQL Server.");
+die(print_r($e));
+}
 ?>
