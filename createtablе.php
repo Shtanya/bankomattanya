ee67cf8925e6a1bc345555e442a232e028fad792
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 21:19:58 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,6 +1,6 @@
 <?php
 try {
-$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = master", "Tanya","L4x78tm2p1");
+$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = bankomat", "Tanya","L4x78tm2p1");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE Karta(
 id INT NOT NULL IDENTITY(1,1)
