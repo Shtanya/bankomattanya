a81a4a3e687216049983c987d5b7d79891f2c10f
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 14:45:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,29 +2,6 @@
 <head>
 </html>
 <Title>Registration Form</Title>
-	<head>
-	<Title>TradeActions</Title>
-	<style type="text/css">
-	body { background-color: 
-	#fff; border-top: solid 10px #000; 
-	color: #333; font-size: .85em; 
-	margin: 20; padding: 20; 
-	font-family: "Segoe UI", 
-	Verdana, Helvetica, Sans-Serif; 
-	} 
-	h1, h2, h3,{ color: #000; 
-	margin-bottom: 0; padding-bottom: 0; } 
-	h1 { font-size: 2em; } 
-	h2 { font-size: 1.75em; } 
-	h3 { font-size: 1.2em; } 
-	table { margin-top: 0.75em; } 
-	th { font-size: 1.2em; 
-	text-align: left; border: none; padding-left: 0; } 
-	td { padding: 0.25em 2em 0.25em 0em; 
-	border: 0 none; } 
-	</style>
-	</head>
-
 <body style="color:Black; background-color:#66cc66"> 
 <img src='http://novostroiki-irkutsk.ru/images/logo_sberbank.png'>    
 </head>
@@ -63,8 +40,8 @@ try {
 $stock1 = $_POST['stock1'];
 $stock2 = $_POST['stock2'];
 $stock3 = $_POST['stock3'];
-$totalstock = 0;
 $buy1 = $_POST['buy1'];
+$totalstock = 0;
 $totalstock = $stock1 + $stock2 + $stock3
 if( isset( $_POST['buy1'] ) )
 try {
