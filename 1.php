6f08c4c10122fe775d2dfb6ec7d5de111500f42b
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 22:18:39 +0300

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,10 +4,12 @@
 </head>
 <body style="color:Black; background-color:#66cc66"> 
 <img src='http://novostroiki-irkutsk.ru/images/logo_sberbank.png'>
+<h1>Выберите операцию которую хотите выполнить</h1>  
 <select name="Выбор операции">
 <option value="Запросить баланс">Запросить баланс
 <option value="Получить наличные">Получить наличные
 </select>
+  
 <form method="post" action="1.php" 
 enctype="multipart/form-data" >
   </form>
