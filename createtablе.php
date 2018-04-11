2aa28d296275ddc0ed9cda12a36228ac14f169f7
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 20:26:32 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -7,15 +7,7 @@ id INT NOT NULL IDENTITY(1,1)
 PRIMARY KEY (id),
 Nomer karti varchar(30),
 Password varchar(30),
-Srok karti varchar(30),
-Balans varchar(30))";
-$sql = "CREATE TABLE Operacii (
-id INT NOT NULL IDENTITY(1,1)
-PRIMARY KEY (id),
-Data DATETIME(30),
-Vremya DATETIME(30),
-Summa varchar(100))";
-)";
+Srok karti varchar(30);
 $conn->query($sql); 
 echo "<h3>Таблица создана!</h3>";
 }
