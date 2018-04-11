cae98bf7473a245b0059864747c0ded6328db27e
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-11 16:46:16 +0300

    Update createtablе.php

--- createtablе.php
+++ createtablе.php
@@ -1,28 +1,29 @@
-<?php  
+<?php
+echo "Снятие наличных"; 
 try { 
 $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = tanya", "Tanya", "Nastyal4x78tm2p1") 
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+CREATE TABLE Karta (
+	Nomer karti varchar(30) NOT NULL,
+	Password varchar(30) NOT NULL,
+	Srok karti varchar(30) NOT NULL,
+	Balans varchar(30) NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
 
-$sql = "CREATE TABLE Karta (
-id int(11) NOT NULL auto_increment,
-Nomer karti varchar(30) NOT NULL AUTO_INCREMENT, 
-Password varchar(30) NOT NULL AUTO_INCREMENT, 
-Srok karti varchar(30) NOT NULL AUTO_INCREMENT, 
-Balans varchar(30) NOT NULL AUTO_INCREMENT,  
-PRIMARY KEY (id)
-) ";
- $sql="CREATE TABLE Operacii (
-id int(11) NOT NULL auto_increment,
-Data DATE(30) NOT NULL,
-Vremya DATETIME(30) NOT NULL,
-Symma varchar(30) NOT NULL,
-Nomer karti varchar(30) NOT NULL AUTO_INCREMENT,
-PRIMARY KEY (id)
-)";
+CREATE TABLE Operacii (
+	Data DATETIME NOT NULL,
+	Vremya DATETIME NOT NULL,
+ Summa varchar NOT NULL,
+	id int NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (id)
+);
 $conn->query($sql); 
 } 
 catch (PDOException $e) { 
 print("Error connecting to SQL Server."); 
 die(print_r($e)); 
 }  
+echo"Снятие наличных";
 ?>
