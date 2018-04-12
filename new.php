8b4db7552ddf8a6efa505c804a321a14ac461597
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-12 12:45:05 +0300

    Update and rename createtablе.php to new.php

--- new.php
+++ new.php
@@ -1,5 +1,5 @@
 <?php
-/*try {
+try {
 $conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = shtt", "tanya", "L4x78tm2p1");
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE Karta(
@@ -14,7 +14,7 @@ echo "<h3>Таблица создана!</h3>";
 catch (PDOException $e) {
 print("Ошибка подключения к SQL Server.");
 die(print_r($e));
-}*/
+}
 ?>
 
-<h3>Таблица создана!</h3>
+
