b878ae03377be2739ea11bf903322d48cb91c570
Shtanya <36952233+Shtanya@users.noreply.github.com>
2018-04-12 14:32:07 +0300

    Update new.php

--- new.php
+++ new.php
@@ -1,13 +1,14 @@
 <?php
 try {
-$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = shtt", "tanya", "Sobachka1");
+$conn = new PDO("sqlsrv:server = tcp:stanya.database.windows.net,1433; Database = shtt", "tanya", "Sobachka1";
 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = "CREATE TABLE Karta(
 id INT NOT NULL IDENTITY(1,1)
 PRIMARY KEY(id),
 Nomerkarti varchar(30),
 Password varchar(30),
-Srokkarti INT)";
+Srokkarti INT,
+Summa INT)";
 $conn->query($sql);
   
 echo "<h3>Таблица создана!</h3>";
